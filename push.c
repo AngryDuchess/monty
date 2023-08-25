f839728f5d943f2013dbcc989bac559ad8107a17
Alli64 <dushimealliance1@gmail.com>
2023-08-24 20:38:48 -0700

    monty

--- push.c
+++ push.c
@@ -2,7 +2,7 @@
 
 /**
  * m_push - pushes an element to the stack
- * stack: pointer to the top of the stack
+ * @stack: pointer to the top of the stack
  * @line_number: line number of code from file read
  */
 void m_push(stack_t **stack, unsigned int line_number)
@@ -10,25 +10,25 @@ void m_push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node;
 	(void) line_number;
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = global_var;
-    new_node->prev = NULL;
+	new_node->n = global_var;
+	new_node->prev = NULL;
 
-    if (*stack == NULL)
-    {
-        new_node->next = NULL;
-        *stack = new_node;
-    }
-    else
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-        *stack = new_node;
-    }
+	if (*stack == NULL)
+	{
+		new_node->next = NULL;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+	}
 }
